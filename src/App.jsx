0e627b31ab18e08e75f18cb5016e940b6451b150
rodrigomarcelo643 <159506698+rodrigomarcelo643@users.noreply.github.com>
2025-07-14 22:54:40 +0800

    Cleaned data

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./pages/Home";
-import OpenSourceChat from "./pages/Open-Source-Chat";
-import Mentorship from "./pages/Mentorship";
-import MentorDashoard from "./pages/Mentor/MentorDashboard";
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/chat",
-    element: <OpenSourceChat />,
-  },
-  {
-    path: "/mentorship",
-    element: <Mentorship />,
-  },
-  {
-    path: "/dashboard",
-    element: <MentorDashoard />,
-  },
+    element: <><div>Test</div></>,
+  }
 ]);
 
 function App() {
