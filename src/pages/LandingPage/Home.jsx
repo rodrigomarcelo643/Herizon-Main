f070522fff5ea3fcb9f605f1e5bc9906fd976519
rodrigomarcelo643 <159506698+rodrigomarcelo643@users.noreply.github.com>
2025-07-15 12:41:08 +0800

    Cleaned ai video for now

--- src/pages/LandingPage/Home.jsx
+++ src/pages/LandingPage/Home.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Navbar from "../../components/Navbar";
-import HeroSection from './sections/HeroSection';
-import SecondSection from './sections/SecondSection';
-import Dialog from '../../components/Dialog';
-import MentorDisplaySection from './sections/MentorDisplaySection';
-<<<<<<< HEAD
+import HeroSection from "./sections/HeroSection";
+import SecondSection from "./sections/SecondSection";
+import Dialog from "../../components/Dialog";
+import MentorDisplaySection from "./sections/MentorDisplaySection";
 import AIVideo from './sections/AIVideo';
-=======
->>>>>>> c2f93779895b6caa88d2d5f560f9d09a26c72f20
-import Footer from '../../components/Footer';
+import Footer from "../../components/Footer";
 export default function Home() {
   const [showLoginDialog, setShowLoginDialog] = useState(false);
   const navigate = useNavigate();
 
   const handleAIClick = () => {
-    navigate('/chat');
+    navigate("/chat");
   };
 
   return (
     <div className="relative">
       <Navbar />
-      <HeroSection 
-        onGetStartedClick={() => setShowLoginDialog(true)} 
-        onAIClick={handleAIClick} 
+      <HeroSection
+        onGetStartedClick={() => setShowLoginDialog(true)}
+        onAIClick={handleAIClick}
       />
       <AIVideo />
       <SecondSection />
-      <MentorDisplaySection/> 
-      <Footer/> 
-      <Dialog isOpen={showLoginDialog} onClose={() => setShowLoginDialog(false)} />
+      <MentorDisplaySection />
+      <Footer />
+      <Dialog
+        isOpen={showLoginDialog}
+        onClose={() => setShowLoginDialog(false)}
+      />
     </div>
   );
 }
