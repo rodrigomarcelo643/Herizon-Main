02845624c0ce1d6d053f4accd4ef60e554edd7dd
Jade Ian Lucero <jame.lucero.swu@phinmaed.com>
2025-07-15 12:49:32 +0800

    add

--- src/pages/LandingPage/Home.jsx
+++ src/pages/LandingPage/Home.jsx
@@ -5,10 +5,8 @@ import HeroSection from './sections/HeroSection';
 import SecondSection from './sections/SecondSection';
 import Dialog from '../../components/Dialog';
 import MentorDisplaySection from './sections/MentorDisplaySection';
-<<<<<<< HEAD
 import AIVideo from './sections/AIVideo';
-=======
->>>>>>> c2f93779895b6caa88d2d5f560f9d09a26c72f20
+impo
 import Footer from '../../components/Footer';
 export default function Home() {
   const [showLoginDialog, setShowLoginDialog] = useState(false);
