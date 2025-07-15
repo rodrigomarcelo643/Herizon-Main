5517be60b86db81c28725875189dcd4e9dfbf840
Jade Ian Lucero <jame.lucero.swu@phinmaed.com>
2025-07-15 12:15:23 +0800

    add

--- src/pages/LandingPage/sections/AIVideo.jsx
+++ src/pages/LandingPage/sections/AIVideo.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+/* import React from 'react'
 import HerizonVideo from '../../../assets/herizon-vid.mp4'
 
-/* function AIVideo() {
+ function AIVideo() {
   return (
     <div className='relative w-full min-h-screen px-50 py-10 '>
         <div className='relative w-full h-140 rounded-4xl border-1 border-[#817773] shadow-2xl shadow-[#817773]'>
