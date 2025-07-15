7e90db0f4121c2d17f642ca28fd80fc2f829b5b6
WhiteCat2023 <berndtdenniscanaya@gmail.com>
2025-07-15 11:38:18 +0800

    feat: mentor header

--- src/pages/MentorPage/MentorPage.jsx
+++ src/pages/MentorPage/MentorPage.jsx
@@ -1,15 +1,24 @@
-
+import Navbar from "../../components/Navbar"
+import { FaSearch } from "react-icons/fa"
 
 export default function Mentor(){
     return(
-        <div className="text-[96px]">
-            <div className="flex justify-between ">
-                <h1 className="font-[Montserrat-Regular]">Mentor</h1>
-                <input type="text" placeholder="Search"/>
-            </div>
-            <div>
+        <>
+            <Navbar/>
+            <div className="px-36">
+                <div className="flex justify-between items-center">
+                    <h1 className="font-[Montserrat-Regular] text-[96px]">Mentor</h1>
+                    <div className="border flex items-center px-2 ps-4 border-[#0000004D] w-[25%] gap-x-2 rounded-full">
+                        <FaSearch className="text-[#0000004D]"/>
+                        <input type="text" placeholder="Search" className="w-full py-3 h-full w-full "/>
+                    </div>
+                    
+                </div>
+                <div>
 
+                </div>
             </div>
-        </div>
+        </>
+        
     )
 }
